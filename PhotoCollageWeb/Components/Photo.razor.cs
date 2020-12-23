ab00df61a01f660274df85780525e5ceb59b485f
Jeremy <492328+jeremyknight-me@users.noreply.github.com>
2020-12-23 12:32:52 -0600

    [web] added fade out on remove (#14)
    * changed where rotation and position were calculated
    
    * added fade out on removal of image
    
    * renamed ImageData to PhotoData; removed unused classes and markup

--- PhotoCollageWeb/Components/Photo.razor.cs
+++ PhotoCollageWeb/Components/Photo.razor.cs
@@ -7,25 +7,8 @@ namespace PhotoCollageWeb.Components
 {
     public partial class Photo
     {
-        [Parameter] public ImageData Image { get; set; }
+        [Parameter] public PhotoData Image { get; set; }
         [Inject] protected IOptions<CollageSettings> Options { get; set; }
         protected CollageSettings Settings => this.Options.Value;
-        protected string CssClasses => string.Empty;
-
-        protected string CssStyles
-        {
-            get
-            {
-                var size = this.Settings.MaximumSize;
-                var styles = $"max-height:{size}px;max-width:{size}px;";
-
-                if (this.Settings.IsGrayscale)
-                {
-                    styles += "filter:grayscale(1);";
-                }
-
-                return styles;
-            }
-        }
     }
 }
--- PhotoCollageWeb/Components/PhotoFrame.razor.cs
+++ PhotoCollageWeb/Components/PhotoFrame.razor.cs
@@ -1,41 +1,28 @@
-﻿using System;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Options;
 using PhotoCollage.Common;
+using PhotoCollage.Common.Enums;
 using PhotoCollageWeb.Models;
 
 namespace PhotoCollageWeb.Components
 {
     public partial class PhotoFrame
     {
-        private int positionLeft = 0;
-        private int positionTop = 0;
-        private int rotation = 0;
-
-        [Parameter] public ImageData Image { get; set; }
+        [Parameter] public PhotoData Image { get; set; }
+        [Parameter] public bool IsRemoved { get; set; } = false;
         [Inject] protected IOptions<CollageSettings> Options { get; set; }
         protected CollageSettings Settings => this.Options.Value;
 
-        protected string CssClasses => "photo-frame bordered";
+        protected string CssClasses => "photo-frame" + this.GetBorderClasses() + this.GetRemovedClasses();
 
-        protected string CssStyles
+        private string GetBorderClasses() => this.Settings.PhotoBorderType switch
         {
-            get
-            {
-                var size = this.Settings.MaximumSize;
-                var half = size / 2;
-                var styles = $"left:calc({this.positionLeft}vw - {half}px);top:calc({this.positionTop}vh - {half}px);transform:rotate({this.rotation}deg);z-index:{this.Image.Count}";
-                return styles;
-            }
-        }
+            BorderType.Border => " bordered",
+            BorderType.BorderHeader => " bordered",
+            BorderType.BorderFooter => " bordered",
+            _ => string.Empty,
+        };
 
-        protected override void OnInitialized()
-        {
-            var random = new Random();
-            this.positionTop = random.Next(0, 100);
-            this.positionLeft = random.Next(0, 100);
-            this.rotation = random.Next(-this.Settings.MaximumRotation, this.Settings.MaximumRotation);
-            base.OnInitialized();
-        }
+        private string GetRemovedClasses() => this.IsRemoved ? " removed" : string.Empty;
     }
 }
