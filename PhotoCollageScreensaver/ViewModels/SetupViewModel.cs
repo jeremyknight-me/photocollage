93d916f86612c1b9c6e6a4d331ab5c5c175091f5
Jeremy <jeremy@theknights.me>
2021-01-11 23:20:23 -0600

    more editor config suggestions

--- PhotoCollageScreensaver/ViewModels/SetupViewModel.cs
+++ PhotoCollageScreensaver/ViewModels/SetupViewModel.cs
@@ -124,7 +124,6 @@ namespace PhotoCollageScreensaver.ViewModels
         {
             var dialog = new FolderBrowserDialog
             {
-                Description = "Select a folder",
                 ShowNewFolderButton = false
             };
             if (!string.IsNullOrWhiteSpace(this.Config.Directory))
@@ -135,7 +134,7 @@ namespace PhotoCollageScreensaver.ViewModels
             var result = dialog.ShowDialog();
             if (result == DialogResult.OK)
             {
-                string path = dialog.SelectedPath;
+                var path = dialog.SelectedPath;
                 if (!string.IsNullOrEmpty(path))
                 {
                     this.SelectedDirectory = path;
@@ -147,17 +146,11 @@ namespace PhotoCollageScreensaver.ViewModels
             }
         }
 
-        public void Save()
-        {
-            this.controller.SaveConfiguration();
-        }
+        public void Save() => this.controller.SaveConfiguration();
 
         // This method is called by the Set accessor of each property.  
         // The CallerMemberName attribute that is applied to the optional propertyName  
         // parameter causes the property name of the caller to be substituted as an argument.  
-        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
