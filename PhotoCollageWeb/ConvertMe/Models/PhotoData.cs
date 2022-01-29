7fa28dd943b0bc0acc25f81506c3c4003315284e
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2022-01-28 18:15:57 -0600

    added back older site

---
+++ PhotoCollageWeb/ConvertMe/Models/PhotoData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PhotoCollage.Common;
+
+namespace PhotoCollageWeb.Models;
+
+public class PhotoData
+{
+    private readonly int index;
+
+    public PhotoData(int count, CollageSettings settings)
+    {
+        this.index = count;
+        this.IsRemoved = false;
+        this.Key = Guid.NewGuid();
+        this.DisplayCssStyles = this.CombineStyles(this.InitializeDisplayStyles(settings));
+        this.PositionCssStyles = this.CombineStyles(this.InitializePositionStyles(settings));
+    }
+
+    public Guid Key { get; }
+    public string Data { get; init; }
+    public string Extension { get; init; }
+    public string DisplayCssStyles { get; }
+    public string PositionCssStyles { get; }
+    public bool IsRemoved { get; internal set; }
+
+    private string CombineStyles(IDictionary<string, string> styles) => string.Join(";", styles.OrderBy(x => x.Key).Select(x => $"{x.Key}:{x.Value}"));
+
+    private IDictionary<string, string> InitializeDisplayStyles(CollageSettings settings)
+    {
+        var styles = new Dictionary<string, string>
+        {
+            { "max-height", $"{settings.MaximumSize}px" },
+            { "max-width", $"{settings.MaximumSize}px" }
+        };
+        if (settings.IsGrayscale)
+        {
+            styles.Add("filter", "grayscale(1)");
+        }
+        return styles;
+    }
+
+    private IDictionary<string, string> InitializePositionStyles(CollageSettings settings)
+    {
+        var random = new Random();
+        var positionTop = random.Next(0, 100);
+        var positionLeft = random.Next(0, 100);
+        var rotation = random.Next(-settings.MaximumRotation, settings.MaximumRotation);
+        var half = settings.MaximumSize / 2;
+
+        return new Dictionary<string, string>
+        {
+            { "left", $"calc({positionLeft}vw - {half}px)" },
+            { "top", $"calc({positionTop}vh - {half}px)" },
+            { "transform", $"rotate({rotation}deg)" },
+            { "z-index", $"{this.index}" }
+        };
+    }
+}
--- PhotoCollageWeb/Server/Program.cs
+++ PhotoCollageWeb/Server/Program.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.ResponseCompression;
+﻿using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
